0cbab2473c09287ba79108b76cbddc1a0056d7d8
Mark Leusink <m.leusink@gmail.com>
2014-02-12 11:46:11 +0100

    corrected case of layout template

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(pjax());
 
 app.use(express.favicon());
 //app.use(express.logger('dev'));
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
